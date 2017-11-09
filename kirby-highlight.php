656c1ad1b371c873adc5aa37de9c0383cfd06bb3
M. Folkers <hello@twobrain.io>
2017-11-09 03:29:11 +0100

    Released v0.2.0-beta :champagne:

--- kirby-highlight.php
+++ kirby-highlight.php
@@ -6,15 +6,14 @@
  * @package   Kirby CMS
  * @author    S1SYPHOS <hello@twobrain.io>
  * @link      http://twobrain.io
- * @version   0.1.0-beta
+ * @version   0.2.0-beta
  * @license   MIT
  */
 
 if(!c::get('plugin.kirby-highlight')) return;
 
 // Initializing composer's autoloader
-require  __DIR__ . DS . 'vendor' . DS . 'autoload.php'; 
+require  __DIR__ . DS . 'vendor' . DS . 'autoload.php';
 
 // Loading core
 require  __DIR__ . DS . 'core' . DS . 'syntax_highlight.php';
-
