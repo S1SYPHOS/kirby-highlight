18d604161454c51aef1738be2d32bf18f4408365
M. Folkers <hello@twobrain.io>
2018-03-09 19:55:45 +0100

    Applying PSR2 coding style

--- core/syntax_highlight.php
+++ core/syntax_highlight.php
@@ -6,26 +6,23 @@ use kirbytext;
 use Highlight\Highlighter;
 use c;
 
-kirbytext::$post[] = function($kirbytext, $value) {
+kirbytext::$post[] = function ($kirbytext, $value) {
+    // Pattern to be matched when parsing kirbytext() (everything between <code> and </code>)
+    $pattern = '~<code[^>]*>\K.*(?=</code>)~Uis';
 
-  // Pattern to be matched when parsing kirbytext() (everything between <code> and </code>)
-  $pattern = '~<code[^>]*>\K.*(?=</code>)~Uis';
+    return preg_replace_callback($pattern, function ($match) {
 
-  return preg_replace_callback($pattern, function($match) {
+        // Instantiating Highlighter & passing array of languages to be auto-detected
+        $highlighter = new Highlighter();
+        $highlighter->setAutodetectLanguages(c::get('plugin.kirby-highlight.languages', ['html', 'php']));
 
-    // Instantiating Highlighter & passing array of languages to be auto-detected
-    $highlighter = new Highlighter();
-    $highlighter->setAutodetectLanguages(c::get('plugin.kirby-highlight.languages', ['html', 'php']));
+        // Optionally escaping each match ..
+        $input = c::get('plugin.kirby-highlight.escaping', false) ? $match[0] : htmlspecialchars_decode($match[0]);
 
-    // Optionally escaping each match ..
-    $input = c::get('plugin.kirby-highlight.escaping', false) ? $match[0] : htmlspecialchars_decode($match[0]);
+        // .. but always highlighting & outputting it
+        $highlightedMatch = $highlighter->highlightAuto($input);
+        $highlightedMatch = $highlightedMatch->value;
 
-    // .. but always highlighting & outputting it
-    $highlightedMatch = $highlighter->highlightAuto($input);
-    $highlightedMatch = $highlightedMatch->value;
-
-    return $highlightedMatch;
-
-  }, $value);
-
+        return $highlightedMatch;
+    }, $value);
 };
